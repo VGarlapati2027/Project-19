84b74a1699ffe97ce5710be79531216c8b777554
VGarlapati2027 <85514686+VGarlapati2027@users.noreply.github.com>
2021-12-05 08:55:28 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var spaceship, meteor, alienship;
+var spaceshipImg, meteorImg, alienshipImg;
+
+
+function preload(){
+spaceshipImg = preload("P20 Spaceship.png");
+}
+
+function setup() {
+ createCanvas(Windowwidth, Windowheight);
+
+}
+
+function draw() {
+ drawSprites();
+ backgroud("black");
+}
